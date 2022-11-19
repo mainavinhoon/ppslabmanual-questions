972a7dca12cb012db225406e638fd593cf0fad9e
Navin <navinrawat11032004@gmail.com>
2022-11-19 18:44:09 +0530

    -A

---
+++ q1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int var1,op,var2;
+
+   cin>>var1>>var2;
+
+    cout<<"Addition  "<<var1+var2<<endl;
+    cout<<"Substraction  "<<var1-var2<<endl;
+    cout<<"multiplications  "<<var1*var2<<endl;
+    cout<<"division  "<<var1/var2<<endl;
+    
+    return 0;
+}
---
+++ q2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a , b , h;
+
+    cin>>b>>h;
+
+    a=0.5*b*h;
+
+    cout << "area of traingle is "<<a;
+    
+    return 0;
+}
